bfe98eccece8d9c43b5a4a8a94b22a0bdf9794d5
vadim <vadimpoldolin19@gmail.com>
2023-08-19 23:46:30 +0200

    finished 3.3

--- src/main/java/com/skypro/SQl_HW33/repository/StudentRepository.java
+++ src/main/java/com/skypro/SQl_HW33/repository/StudentRepository.java
@@ -4,6 +4,9 @@ import com.skypro.SQl_HW33.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface StudentRepository extends JpaRepository<Student,Long> {
+    List<Student> findAllByAge(int age);
 }
