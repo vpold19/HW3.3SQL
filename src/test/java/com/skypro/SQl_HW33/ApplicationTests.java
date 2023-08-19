d2de8cb2b2d77f05dc656b9105ef6996be8c106e
vadim <vadimpoldolin19@gmail.com>
2023-08-19 22:14:47 +0200

    first commit

---
+++ src/test/java/com/skypro/SQl_HW33/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.SQl_HW33;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
